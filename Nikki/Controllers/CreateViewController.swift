82a1fe9e35d73cc2133e2bbcfca4a5a6fc80035b
yuuta <yuuta_cookie@icloud.com>
2019-12-12 08:13:25 +0900

    記事作成画面とCreateViewControllerを作成。ホーム画面と記事作成画面はsegueで繋ぎ、returnButtonも実装。

---
+++ Nikki/Controllers/CreateViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateViewController.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/12.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func returnButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
