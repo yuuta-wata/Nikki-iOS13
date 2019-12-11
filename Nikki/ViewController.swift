c47f9348067ee9c73388dc00f9746953edb86f55
yuuta <yuuta_cookie@icloud.com>
2019-12-12 06:02:39 +0900

    Initial Commit

---
+++ Nikki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/12.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
