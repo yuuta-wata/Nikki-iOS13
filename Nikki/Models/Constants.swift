c56cf894e1018599a811dd06cec85cfa90d80744
yuuta <yuuta_cookie@icloud.com>
2019-12-12 06:14:45 +0900

    ViewControllerを削除し、TableViewControllerとTabBarControllerを作成。同時にIdentifierを格納するConstantsファイルを作成。

---
+++ Nikki/Models/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/12.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ItemCell"
+}
