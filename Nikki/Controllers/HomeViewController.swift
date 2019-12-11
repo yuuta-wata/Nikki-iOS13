5208786b8f99287b0c6c7540ef0f66947e2ded9f
yuuta <yuuta_cookie@icloud.com>
2019-12-12 07:27:05 +0900

    TableViewControllerにセルを表示させる実装を作成。

--- Nikki/Controllers/HomeViewController.swift
+++ Nikki/Controllers/HomeViewController.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class HomeViewController: UITableViewController {
+    
+    var itemArray = ["テスト"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - TableView Datasource Methods
+    // セルを表示する行数を取得
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    // セルを作成
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用するセルを作成
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        // 配列から値を取得
+        let item = itemArray[indexPath.row]
+        //　セルに値を表示する
+        cell.textLabel?.text = item
+        
+        return cell
+    }
+    
+    // MARK: - TbaleView Delegate Methods
+    // セルが選択されていることを通知する
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 選択したセルにアニメーションをつける
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Add New Items
 
 
 }
