7bd791a952692acdfee8d7dfc7e405a5863c76a8
yuuta <yuuta_cookie@icloud.com>
2019-12-13 11:33:05 +0900

    Articleクラスを作成し、contentデータを追加。 Categoryクラスを作成し、titleデータを追加。

---
+++ Nikki/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/13.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Article: Object {
+    dynamic var content: String = ""
+}
---
+++ Nikki/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/13.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    dynamic var title: String = ""
+}
