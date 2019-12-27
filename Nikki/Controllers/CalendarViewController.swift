78331bed5b59255711e2d85b6c300303f5e8c753
yuuta <yuuta_cookie@icloud.com>
2019-12-27 11:50:17 +0900

    FSCalendarをインストールし、カレンダー画面とCalendarViewControllerを作成。

---
+++ Nikki/Controllers/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalenderViewController.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/27.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
