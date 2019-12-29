27e9fddf9678ba4b5d3ba268b7e80f86219b24bf
yuuta <yuuta_cookie@icloud.com>
2019-12-29 22:32:00 +0900

    カレンダーの年号と曜日を日本表記に変更

--- Nikki/Controllers/CalendarViewController.swift
+++ Nikki/Controllers/CalendarViewController.swift
@@ -10,19 +10,27 @@ import UIKit
 import FSCalendar
 import CalculateCalendarLogic
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate {
+    let dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"]
+    var weekCount = 0
 
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar.dataSource = self
+        calendar.delegate = self
+        // 年月を日本表記に変更
+        calendar.appearance.headerDateFormat = "y年MM月"
+        
+        settingWeek()
+    }
+    // 曜日を日本表記に変更
+    private func settingWeek() {
+        for i in 0...6 {
+            calendar.calendarWeekdayView.weekdayLabels[i].text = dayOfWeek[weekCount]
+            weekCount += 1
+        }
     }
 }
 
-extension CalendarViewController: FSCalendarDataSource {
-    private func formatter() {
-        let jpCalendar = Calendar(identifier: .gregorian)
-        let date = jpCalendar.dateComponents([.year, .month ], from: Date())
-        
-    }
-}
