6c4a8a9384c61910111742d37869e7c1c3d9b726
yuuta <yuuta_cookie@icloud.com>
2019-12-29 21:00:11 +0900

    祝日判定をしてくれるCalculateCalendarLogicをインストール

--- Nikki/Controllers/CalendarViewController.swift
+++ Nikki/Controllers/CalendarViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 import FSCalendar
+import CalculateCalendarLogic
 
 class CalendarViewController: UIViewController {
 
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension CalendarViewController: FSCalendarDataSource {
+    private func formatter() {
+        let jpCalendar = Calendar(identifier: .gregorian)
+        let date = jpCalendar.dateComponents([.year, .month ], from: Date())
+        
     }
 }
