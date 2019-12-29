63a50556ffb66f77bf65c2ac798601002bf657be
yuuta <yuuta_cookie@icloud.com>
2019-12-29 19:21:52 +0900

    カレンダーを自作しようとしたが、現地点の技術力じゃ間に合わない為、断念。FSCalendarを再インストール。

---
+++ Nikki/Controllers/CalendarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewController.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/29.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
