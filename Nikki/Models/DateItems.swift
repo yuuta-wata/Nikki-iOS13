0ba767a767bdc7f972f46ef6c33d6c65d2e9b93d
yuuta <yuuta_cookie@icloud.com>
2019-12-30 18:06:42 +0900

    日付を年月日で取得する列挙体を作成

---
+++ Nikki/Models/DateItems.swift
@@ -0,0 +1,26 @@
+//
+//  Date.swift
+//  Nikki
+//
+//  Created by 渡辺雄太 on 2019/12/30.
+//  Copyright © 2019 Yuta Watanabe. All rights reserved.
+//
+
+import Foundation
+
+// 日付を年、月、日で取得する
+enum DateItems {
+    struct Request {
+        var year: Int
+        var month: Int
+        var day: Int
+        // 初期化
+        init() {
+            let calendar = Calendar(identifier: .gregorian)
+            let date = calendar.dateComponents([.year, .month, .day], from: Date())
+            year = date.year!
+            month = date.month!
+            day = date.day!
+        }
+    }
+}
